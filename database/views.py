f7b740b46765c7710fa0de50cd1938ba7db76328
Taqvis <ataqvi2010@gmail.com>
2021-09-18 14:43:38 -0400

    bringing in patient content

--- database/views.py
+++ database/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+patients = [
+    {
+        'name': 'John Smith',
+        'DOB': 'April 4, 2002',
+        'sex': 'Male',
+        'blood_pressure': 'High',
+        'risk_factor': 0.3
+    },
+    {
+        'name': 'Jane Doe',
+        'DOB': 'September 20, 1994',
+        'sex': 'Female',
+        'blood_pressure': 'Moderate',
+        'risk_factor': 0.1
+    }
+]
 
 def home(request):
-    return HttpResponse('<h1>Database Home</h1>')
+    context = {
+        'patients': patients
+    }
+    return render(request, 'database/patients.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Database About</h1>')
+    return render(request, 'database/about.html')
