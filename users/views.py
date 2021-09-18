04daa87c11b5133dbb36d127b8634207ad9f9ccb
Taqvis <ataqvi2010@gmail.com>
2021-09-18 16:53:33 -0400

    html updated

--- users/views.py
+++ users/views.py
@@ -4,8 +4,6 @@ from django.contrib.auth.forms import UserCreationForm
 def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
-        if form.is_valid():
-            
     else:
         form = UserCreationForm()
     return render(request, 'users/register.html', {'form': form})
