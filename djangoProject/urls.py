160430a5520f40317447248fa958fbdda18b1065
Taqvis <ataqvi2010@gmail.com>
2021-09-18 12:20:51 -0400

    allowing html access

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('database/', include('database.urls')),
-    path('', include('database.urls'))
+    path('', include('landing.urls'))
 ]
