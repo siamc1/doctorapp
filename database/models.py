746efbd9cf681cd34af66752a3657d61c1553179
Taqvis <ataqvi2010@gmail.com>
2021-09-19 07:04:12 -0400

    reading csv files

--- database/models.py
+++ database/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import csv
 
 class Patient(models.Model):
     name = models.CharField(max_length = 30)
@@ -6,3 +7,14 @@ class Patient(models.Model):
     sex = models.CharField(max_length = 10)
     blood_pressure = models.CharField(max_length = 10)
     risk_factor = models.FloatField()
+
+with open("csv/Tony.csv") as f:
+    reader = csv.reader(f)
+    for row in reader:
+        _, created = Patient.objects.get_or_create(
+            name=row[0],
+            risk_factor=row[1],
+            sex=row[3],
+            blood_pressure=row[4],
+            DOB=row[5],
+        )
